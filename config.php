9b72c8b515a2a8da7948f63766436c739f47d7df
Bill Ferster <bferster@stagetools.com>
2017-07-27 20:12:28 -0400

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $dbms = 'mysql';
 $dbhost = 'localhost';
 $dbname = 'qmedia';
 $dbuser = 'qmedia';
-$dbpasswd = '1990qmedia';
+$dbpasswd = '';  // Supply your own prename
 $connection = mysql_connect($dbhost, $dbuser, $dbpasswd) or die ("Could not connect to server.");
 $dc = mysql_select_db($dbname,$connection) or die ("Couldn't select database.");
 ?>
