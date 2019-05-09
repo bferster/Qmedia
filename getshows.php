4a02f9a357a1719a684889a40c91c0437e95f87a
Bill Ferster <bferster@virginia.edu>
2019-05-09 14:11:47 -0400

    php7 register getshows

--- getshows.php
+++ getshows.php
@@ -2,7 +2,9 @@
 header('Cache-Control: no-cache, no-store, must-revalidate'); 
 header('Expires: Sun, 01 Jul 2005 00:00:00 GMT'); 
 header('Pragma: no-cache'); 
-require_once('config.php');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+require_once('config7.php');
 			
 	$query="SELECT * FROM qshow";								// Query start
 	if (isSet($_GET['email'])) {		 						// If set
@@ -13,33 +15,35 @@ require_once('config.php');
 		print("Sorry, there are no projects with this email\n");// Return error
 		return;
 		}
-	$result=mysql_query($query);								// Query
-	if ($result == false) {										// Bad query
+	$result=mysqli_query($link, $query);						// Run query
+		if ($result == false) {									// Bad query
 		print("Error getting projects");						// Return error
+		mysqli_free_result($result);							// Free
+		mysqli_close($link);									// Close session
 		exit();													// Quit
 		}
 	$pass=strtolower(addslashes($_GET['pass']));				// Get pass option
 	$body="Here are projects saved under the email ".$email.":\n\n";// Header
-	$num=mysql_numrows($result);								// Get num rows
 	if ($pass == 5) {											// Getting folio password
 		$body="Here is your password for ".$email.":\n\n";		// Header
-		for ($i=0;$i<$num;$i++) {								// For each row
-			if (mysql_result($result,$i,"version") == 5)		// If folio
-				$body.="Password: ".mysql_result($result,$i,"password")."\n";	// Add password
+		while ($row=mysqli_fetch_assoc($result)) {				// Loop through rows
+			if ($row["version"] == 5)							// If folio
+				$body.="Password: ".$row["password"]."\n";		// Add password
 			}
 		}			
 	else{
-		for ($i=0;$i<$num;$i++) {								// For each row
-			$body.="Id:" .mysql_result($result,$i,"id")."\t";			// Add id
-			$body.="Priv: ".mysql_result($result,$i,"private")."\t";	// Add private
-			$body.="Del: ".mysql_result($result,$i,"deleted")."\t";		// Add Deleted
-			$body.="Date: ".mysql_result($result,$i,"date")."\t";		// Add Date
-			$body.="Pass: ".mysql_result($result,$i,"password")."\t";	// Add password
-			$body.="Title: ".mysql_result($result,$i,"title")."\n";		// Add title
+		while ($row=mysqli_fetch_assoc($result)) {				// Loop through rows
+			$body.="Id:" .$row["id"]."\t";						// Add id
+			$body.="Priv: ".$row["private"])."\t";				// Add private
+			$body.="Del: ".$row["deleted"]."\t";				// Add Deleted
+			$body.="Date: ".$row["date"]."\t";					// Add Date
+			$body.="Pass: ".$row["password"]."\t";				// Add password
+			$body.="Title: ".$row["title"]."\n";				// Add title
 			}		
 		}
 	
-	mysql_close();												// Close session
+	mysqli_free_result($result);								// Free
+	mysqli_close($link);										// Close session
 	ini_set("sendmail_from",$email);							// Close
 	$sub="Here are your saved projects...";						// Subject			
 	mail($email,$sub,$body,"From: $email\nReply-To: $email\n");	// Mail it
